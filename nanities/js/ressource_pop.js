8abe5f5566c7fca0a157d4efd5bdd02946e4d8dd
duldrome <t.dulitz@gmx.de>
2013-07-29 13:56:18 +0200

    Update ressource_pop.js

--- nanities/js/ressource_pop.js
+++ nanities/js/ressource_pop.js
@@ -1,51 +1,79 @@
-var Field_Width = 100;
-var Field_Height = 100;
-var Startpos_X = Math.floor(Field_Width/2);  	//abrunden
-var Startpos_Y = Math.floor(Field_Height/2);	//abrunden
+var FieldWidth = 100;
+var FieldHeight = 100;
+var StartposX = Math.floor(FieldWidth/2);		//abrunden
+var StartposY = Math.floor(FieldHeight/2);	//abrunden
 var OUR_MAIN_PLAYGROUND_ARRAY;
-fInit_Deposits("iron", 0.1, 0.3, 20, 1, 5, 0.5);
+InitDeposits("iron", 0.1, 0.3, 20, 1, 5, 0.5);
 
-function fInit_Deposits(	resource_type,		//Ressourcentyp - ID oder Name
-							radius_start,		//innerer Kreisradius ("ab hier darf ressource spawnen")
-							radius_stop,		//äußerer Kreisradius ("bis hier darf ressource spawnen")
-							resource_amount,	//gesamte Anzahl an Ressourcenfeldern
-							deposit_size_min,	//miteinander verknüpfte Ressourcen - noch nicht implementiert
-							deposit_size_max)	//miteinander verknüpfte Ressourcen - noch nicht implementiert
+function InitDeposits(		resourceType,	//Ressourcentyp - ID oder Name
+							radiusStart,	//innerer Kreisradius ("ab hier darf ressource spawnen")
+							radiusStop,		//äußerer Kreisradius ("bis hier darf ressource spawnen")
+							resourceAmount,	//gesamte Anzahl an Ressourcenfeldern
+							depositSizeMin,	//miteinander verknüpfte Ressourcen - noch nicht implementiert
+							depositSizeMax, //miteinander verknüpfte Ressourcen - noch nicht implementiert
+							offset)			//kreisverfeinerung
 {
 	//array mit feldern für ressource erzeugen
-	var radius_min = Math.floor(radius_start*Field_Width);
-	var radius_max = Math.floor(radius_stop*Field_Height);
-	var Ressource_Array = new Array(ressource_amount);
-	var OptionalFields_Array = new Array();
-	OptionalFields_array = fAvailable_for_Resource(radius_min, radius_max, 0.5);
-	
-	
-	
-	//felder aus array in spielfeld eintragen
-	//return true
+	var radiusMin = Math.floor(radiusStart*FieldWidth);
+	var radiusMax = Math.floor(radiusStop*FieldHeight);
+	var RessourceArray = new Array(ressourceAmount);
+	var OptionalFieldsArray = new Array();
+	OptionalFieldsArray = AvailableForResource(radiusMin, radiusMax, offset);
+	CreateDeposits(OptionalFieldsArray, resourceType, resourceAmount);
 }
 
 //	******************************************************************************************************
-//	fAvailable_for_Resource creates and returns array of IDs of fields who are within minimum and maximum range
+//	AvailableForResource creates and returns array of Coordinates of fields who are within minimum and maximum range
 //	offset polishs the circleshape
 //	iterates through all Fields that are within a square for startposition XY minus and plus radius
 //	distance is calculated with euclidean distance formula
 //	returns the array
 //	******************************************************************************************************
-function fAvailable_for_Resource(radius_min, radius_max, offset)
+function AvailableForResource(radiusMin, radiusMax, offset)
 {
-	var aAvailable = new Array();
+	var Available = new Array();
 	var distance;
-	for (var i = (Startpos_X - radius_max) ; i <= (Startpos_X + radius_max) ; i++)
+	for (var i = (StartposX - radiusMax) ; i <= (StartposX + radiusMax) ; i++)
 	{
-		for (var j = (Startpos_Y - radius_max) ; j <= (Startpos_Y + radius_max) ; j++)
+		for (var j = (StartposY - radiusMax) ; j <= (StartposY + radiusMax) ; j++)
 		{
-			distance = Math.sqrt((i-Startpos_X)*(i-Startpos_X) + (j-Startpos_Y)*(j-Startpos_Y)); //euclid
-			if (distance > (radius_min+offset) && distance < (radius_max+offset))
+			distance = Math.sqrt((i-StartposX)*(i-StartposX) + (j-StartposY)*(j-StartposY)); //euclid
+			if (distance > (radiusMin+offset) && distance < (radiusMax+offset))
 			{
-				aAvailable.push(OUR_MAIN_PLAYGROUND_ARRAY[i,j].id);
+				Available.push([i,j]);
 			}
 		}
 	}
-	return aAvailable;
+	return Available;
+}
+
+//	******************************************************************************************************
+//	creates array of resourceAmount random nonrepeating numbers between 0 and OptionalFieldsArray.length
+//	injects these fieldcoordinates into the main matrix
+//	******************************************************************************************************
+function CreateDeposits(OptionalFieldsArray, resourceType, resourceAmount)
+{
+	var resourceField = new Array();
+	var exist = false;
+    var random;
+    for (var i=0;i<resourceAmount;)
+    {
+		random=Math.floor(Math.random()*(OptionalFieldsArray.length));
+		for (var j=0;j<resourceAmount;j++)
+		{
+			if (resourceField[j] == random) exist = true; 
+		}
+		if (!exist) 
+		{
+			resourceField[i]=random;
+			i++;
+		}
+		exist = false;
+	}
+	/*
+	for (var k=0;k<(resourceField.length);k++)
+	{
+		OUR_MAIN_PLAYGROUND_ARRAY[OptionalFieldArray[resourceField[k]][0]][OptionalFieldArray[resourceField[k]][1]].klassehinzufügen(resourceType);
+	}
+	*/
 }
