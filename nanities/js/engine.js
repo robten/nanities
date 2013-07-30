cf2d75cc8d1b6ffd9444961e177de3436c7f4e3f
Robten <robert.seemel@gmail.com>
2013-07-30 11:01:20 +0200

    tuning modules into objects: engine.js

--- nanities/js/engine.js
+++ nanities/js/engine.js
@@ -1,14 +1,21 @@
-// The Playgound as an two-dimensional Array of Objects / Arrays of Objects
-var matrix = new Array;
-for (var x=0;x<10;x++) {
-	matrix[x] = new Array;
-	for (var y=0;y<10;y++) {
-		matrix[x][y] = null;
+var Engine = {
+
+	// The Playgound model as an two-dimensional Array of
+	// Objects / Arrays of Objects
+	matrix: new Array,
+
+	matrixInit: function() {
+		for (var x=0;x<10;x++) {
+			this.matrix[x] = new Array;
+			for (var y=0;y<10;y++) {
+				this.matrix[x][y] = null;
+			}
+		}
+	},
+	// The Event Loop as central unit. It controls model&view and deals
+	// with communication of ingame-objects.
+	evLoop: function() {
+		// let's wait for events and respond to them.	
 	}
 }
 
-// The Event Loop as central unit. It controls model&view and deals
-// with communication of ingame-objects.
-function evLoop() {
-	// let's wait for events and respond to them.	
-}
--- nanities/js/main.js
+++ nanities/js/main.js
@@ -1,6 +1,7 @@
 // Test some cells:
-matrix[1][1] = new Entity(true);
-matrix[4][4] = new Actor("Otto","opponent");
-inspectMatrix(0,0,matrix[0][0]);
-inspectMatrix(1,1,matrix[1][1]);
-inspectMatrix(4,4,matrix[4][4]);
+Engine.matrixInit();
+Engine.matrix[1][1] = new Entity(true);
+Engine.matrix[4][4] = new Actor("Otto","opponent");
+inspectMatrix(0,0,Engine.matrix[0][0]);
+inspectMatrix(1,1,Engine.matrix[1][1]);
+inspectMatrix(4,4,Engine.matrix[4][4]);
