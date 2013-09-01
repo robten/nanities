bb1be9d9690dbe86aa3dbe02b828972c185c8ee5
robten <robert.seemel@gmail.com>
2013-09-01 08:54:58 +0200

    Slightly changed textures of testing.png; Switched Grid off by default.

--- nanities/js/main.js
+++ nanities/js/main.js
@@ -2,11 +2,12 @@ nanities.engine.setModel(new nanities.Model(60, 60));
 nanities.resources.initDepots("*",0.1,0.3,20,2,5,0.5);
 var testView = new nanities.View("canvas1");
 testView.setModel(nanities.engine.model());
-testView.setShowGrid(true);
+testView.setShowGrid(false);
 //tests.showFullMatrix();
 
 var gridToggleButton = document.getElementById("gridToggle");
-var toggle = true;
+var toggle = false;
+gridToggleButton.innerHTML = "Grid ON";
 gridToggleButton.onclick = function() {
 	toggle = !toggle;
 	testView.setShowGrid(toggle);
